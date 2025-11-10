2ab30324c34ebd0d873c9eea9ed9737e429dd198
sukant43 <sukant98657@gmail.com>
2025-11-10 22:11:28 +0530

    view

---
+++ HelloWorld/App.js
@@ -0,0 +1,9 @@
+import {View} from 'react-native';
+
+export default function App(){
+  return(
+<View style={{flex:1,backgroundColor:"plum"}}>
+  <View style={{height:100,width:100,backgroundColor:"blue"}}>Hello</View>
+</View>
+  );
+}
