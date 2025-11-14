9ec90680a9092416b4293c1d7335f1d3baed4737
sukant43 <sukant98657@gmail.com>
2025-11-14 12:01:30 +0530

    scroll view

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,16 +1,17 @@
-import {View,Text,Image, ImageBackground} from 'react-native';
+import {View,Text,Image, ImageBackground, ScrollView} from 'react-native';
 
 export default function App(){
   return(
-<View style={{flex:1,backgroundColor:"plum",padding:60}}>
+<ScrollView style={{flex:1,backgroundColor:"plum",padding:60}}>
   {/* <Text><Text style={{color:"white"}}>Hello</Text> World</Text>
   <Image source={{uri:"https://picsum.photos/id/237/200/300"}}
     style={{height:200,width:200}}
   ></Image> */}
-  <ImageBackground source={{uri:"https://picsum.photos/id/237/200/300"}} style={{flex:1}}>
+  <ImageBackground source={{uri:"https://picsum.photos/id/237/200/300"}} style={{flex:1, height:200,width:200}}/>
 <Text>Hello</Text>
-  </ImageBackground>
+<Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+</Text>
 
-</View>
+</ScrollView>
   );
 }
