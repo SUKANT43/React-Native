6092d13dd00354768b2313ed7bf892372ab26d7d
sukant43 <sukant98657@gmail.com>
2025-11-14 11:49:33 +0530

    imageBackground

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,9 +1,16 @@
-import {View,Text} from 'react-native';
+import {View,Text,Image, ImageBackground} from 'react-native';
 
 export default function App(){
   return(
 <View style={{flex:1,backgroundColor:"plum",padding:60}}>
-  <Text><Text style={{color:"white"}}>Hello</Text> World</Text>
+  {/* <Text><Text style={{color:"white"}}>Hello</Text> World</Text>
+  <Image source={{uri:"https://picsum.photos/id/237/200/300"}}
+    style={{height:200,width:200}}
+  ></Image> */}
+  <ImageBackground source={{uri:"https://picsum.photos/id/237/200/300"}} style={{flex:1}}>
+<Text>Hello</Text>
+  </ImageBackground>
+
 </View>
   );
 }
