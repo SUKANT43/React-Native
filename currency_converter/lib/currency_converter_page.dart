09b7268ac90439db8e802714589a5c87b3090ff4
sukant43 <sukant98657@gmail.com>
2025-12-23 20:31:12 +0530

    weather app starter

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -1,65 +1,91 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget{
+class CurrencyConverterMaterialPage extends StatefulWidget {
   const CurrencyConverterMaterialPage({super.key});
-@override
+
+  @override
+  State<CurrencyConverterMaterialPage> createState() =>
+      _CurrencyConverterMaterialPageState();
+}
+
+class _CurrencyConverterMaterialPageState
+    extends State<CurrencyConverterMaterialPage> {
+
+  int result = 0;
+  final TextEditingController tx = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-          final border=OutlineInputBorder(
-              borderSide: BorderSide(
-                width: 2.0,
-                style: BorderStyle.solid,
-              ),
-              borderRadius: BorderRadius.circular(60),
-            );
 
-    return  Scaffold(
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(width: 2.0),
+      borderRadius: BorderRadius.circular(60),
+    );
 
+    return Scaffold(
       backgroundColor: Colors.blueGrey,
-      body:Center(
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-        Container(  
-          //color: Colors.black,
-          padding: EdgeInsets.all(10),
-          margin: EdgeInsets.all(10),
-          child:const Text("0",
-          style: TextStyle(
-            fontSize: 35,
-            fontWeight: FontWeight.bold,
-            color: Color.fromARGB(255, 249, 249, 250)
-          ),
-        ),)
-        ,
-        Container(
-          padding: const EdgeInsets.all(8.0),
-          margin: EdgeInsets.all(5.0),
-       child: TextField(
-          style: TextStyle(
-            color: Color.fromRGBO(2, 0, 11, 1),
-            fontWeight: FontWeight.bold
-          ),
-          decoration: InputDecoration(
-            hintText: "Please enter amount in USD",
-            hintStyle: TextStyle(
-              fontWeight:FontWeight.normal,
-              color: Colors.black
-            ),
-            suffixIcon:Icon(Icons.monetization_on ),
-            suffixIconColor: Colors.black,
-            filled: true,
-            fillColor: Colors.white,
-            focusedBorder:border,
-            enabledBorder: border
-          ),
-          keyboardType: const TextInputType.numberWithOptions(
-            decimal: true,
-          ),
-        ),
-        ),
-        ],
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey,
+        elevation: 0,
+        title: const Text("Currency Converter"),
+        centerTitle: true,
+        foregroundColor: Colors.white,
       ),
-    ),
-  );
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Text(
+              result.toString(),
+              style: const TextStyle(
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.all(5.0),
+              child: TextField(
+                controller: tx,
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                decoration: InputDecoration(
+                  hintText: "Please enter amount in USD",
+                  suffixIcon: const Icon(Icons.monetization_on),
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+              ),
+            ),
+
+            TextButton(
+              onPressed: () {
+                if (kDebugMode) {
+                  print("button clicked");
+                }
+
+                setState(() {
+                  result = ((double.tryParse(tx.text) ?? 0) * 81).toInt();
+                });
+              },
+              style: const ButtonStyle(
+                backgroundColor:
+                    MaterialStatePropertyAll(Colors.black),
+              ),
+              child: const Text(
+                "Convert",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
