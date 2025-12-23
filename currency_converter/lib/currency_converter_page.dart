378628df88a8bb7eb58f138b23097684482ac9b1
sukant43 <sukant98657@gmail.com>
2025-12-23 19:19:14 +0530

    padding margin container

--- currency_converter/lib/currency_converter_page.dart
+++ currency_converter/lib/currency_converter_page.dart
@@ -19,14 +19,22 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
          child: Column(
            mainAxisAlignment: MainAxisAlignment.center,
           children: [
-          Text("0",
+        Container(  
+          //color: Colors.black,
+          padding: EdgeInsets.all(10),
+          margin: EdgeInsets.all(10),
+          child:const Text("0",
           style: TextStyle(
             fontSize: 35,
             fontWeight: FontWeight.bold,
             color: Color.fromARGB(255, 249, 249, 250)
           ),
-        ),
-        TextField(
+        ),)
+        ,
+        Container(
+          padding: const EdgeInsets.all(8.0),
+          margin: EdgeInsets.all(5.0),
+       child: TextField(
           style: TextStyle(
             color: Color.fromRGBO(2, 0, 11, 1),
             fontWeight: FontWeight.bold
@@ -44,6 +52,10 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
             focusedBorder:border,
             enabledBorder: border
           ),
+          keyboardType: const TextInputType.numberWithOptions(
+            decimal: true,
+          ),
+        ),
         ),
         ],
       ),
