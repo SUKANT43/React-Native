d07727fc91f721dfdd96f58c66dd50040f9faa2f
sukant43 <sukant98657@gmail.com>
2026-03-13 19:41:53 +0530

    Text, View & Image Components

--- myApp/app/index.jsx
+++ myApp/app/index.jsx
@@ -1,10 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View,Image } from 'react-native'
+
+import Logo from "../assets/images/logo_dark.png"
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+        <Image source={Logo} style={styles.img}/>
+      <Text style={styles.title}>The Number 1</Text>
+
+      <Text style={{marginTop:10,marginTop:30}}>Reading List App</Text>
+
+       
+
     </View>
   )
 }
@@ -12,5 +19,18 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
-    
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    title:{
+        fontWeight:'bold',
+        fontSize:18
+    },
+   
+    img:{
+        marginVertical:20,
+        backgroundColor:"black"
+    }
 })
